dc3d5c26065799eb878712212e4c6161d7bc7e81
kurniadikevin <kurniadikevin@gmail.com>
2022-08-07 19:51:21 +0700

    scripting done and added few styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import React, {useState,useEffect} from 'react';
 function App() {
 
  const [score,setScore] = useState(0);
+ const [bestScore, setBestScore] = useState(0);
  const [arrImageName, setArrImageName ] = useState(['chilling','coding','flying','hanging','jugling','jumping','reachmoon','rocketing','sitting','standmoon'])
+ const [selected, setSelected] = useState([]);
 
  let haveNum =[];
   
@@ -23,21 +25,33 @@ function App() {
           }
       }
 
+
       const arrShuffle = function(){
         setArrImageName(
-        arrImageName.sort(() => Math.random() - 0.5)
-        )
-        alert(arrImageName);
+          arrImageName.sort(() => Math.random() - 0.5)
+          )
+        //alert(arrImageName);
         setScore((prev) => prev + 1 ) 
       }
       
+      // check if user click the same image source as before
+      const gameRule = function(ev){
+        let targetSrc = ev.target.src
+        setSelected( (prev) => [...prev, targetSrc]);
+        //alert('image src' +selected);
+        if( selected.includes(targetSrc) ){
+          alert(`game over your score is ${score}`);
+          setBestScore(score);
+          setScore(-1);
+          setSelected([]);
+        }
+      }
 /*
       useEffect(()=>{
         alert('use effect run')
         
       },[arrImageName]);
      */
-
 
   return (
     <div className="App">
@@ -46,32 +60,33 @@ function App() {
           <div className='title'>Memory Card</div>
           <div className='score-board'>
                 <div>Score : {score}</div>
-                <div> Best Score : </div>
+                <div> Best Score : {bestScore}</div>
           </div>
       </div>
 
       <div className='main-row'>
           <div className='img-row1'>
                   <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'
-                   onClick={arrShuffle} />
+                   onClick={ (event)=>{gameRule(event); arrShuffle();} }  />
+
                   <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'
-                  onClick={arrShuffle}/>
+                  onClick={(event)=>{gameRule(event); arrShuffle();}}/>
                   <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'
-                  onClick={arrShuffle}/>
+                  onClick={(event)=>{gameRule(event); arrShuffle();}}/>
           </div>
 
           <div className='img-row2'>
                   <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'
-                  onClick={arrShuffle}/>
+                  onClick={(event)=>{gameRule(event); arrShuffle();}}/>
                   <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'
-                  onClick={arrShuffle}/>
+                  onClick={(event)=>{gameRule(event); arrShuffle();}}/>
                   <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'
-                  onClick={arrShuffle}/>    
+                  onClick={(event)=>{gameRule(event); arrShuffle();}}/>    
          </div>
-
+        
       </div>
 
-    <p> test state: {arrImageName} {score}</p>
+    
     </div>
   );
 }
