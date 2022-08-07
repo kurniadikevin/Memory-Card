7b9d9f6d07cc02cc32f8444be42d4ea4664f57ac
kurniadikevin <kurniadikevin@gmail.com>
2022-08-07 12:41:26 +0700

    add score and image display in random

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, {useState} from 'react';
 
 function App() {
+
+ const [score,setScore] = useState();
+ const arrImageName = ['chilling','coding','flying','hanging','jugling','jumping','reachmoon','rocketing','sitting','standmoon'];
+ 
+// const randomNum = Math.floor(Math.random() * 10);
+
+ let haveNum =[];
+  
+   function randomNumFunc(){
+      const randomNum = Math.floor(Math.random() * 10);
+          if(!haveNum.includes(randomNum)){
+              haveNum.push(randomNum);
+              return randomNum;
+          } else {
+            if(haveNum.length < 10){
+              return randomNumFunc()
+            } else {
+              alert('there is no more number');
+              return false
+            }
+          }
+      }
+
+  
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='head-row'>
+          <div className='title'>Memory Card</div>
+          <div className='score-board'>
+                <div>Score : {score}</div>
+                <div> Best Score : </div>
+          </div>
+      </div>
+
+      <div className='main-row'>
+          <div className='img-row1'>
+                  <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'/>
+                  <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'/>
+                  <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'/>
+          </div>
+
+          <div className='img-row2'>
+                  <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'/>
+                  <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'/>
+                  <img src={require(`./image/${arrImageName[randomNumFunc()]}.jpg`)} alt='chilling' width='200' height='200'/>    
+         </div>
+
+      </div>
+
     </div>
   );
 }
